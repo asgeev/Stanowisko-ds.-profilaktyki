84eb5b72a2d18173cf3a83643e178909ca4af16e
polishghost27 <adamszymanski27@hotmail.com>
2021-11-11 21:23:47 +0100

    Add hamburger and employee div

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+const employees = [ "Adam Szymanski", "Łukasz Bondyra"]
+
+
+/*Add name employee*/
+document.getElementById("employees").innerHTML = " " + employees[0]
+
+
+
+/*Hamburger click*/
+const hamburger = document.querySelector('.hamburger');
+
+const handleClick = () => {
+    hamburger.classList.toggle('hamburger--active');
+}
+
+hamburger.addEventListener('click', handleClick);
